39d1574f1b890eaa9a786112b2abbe7fddefbd7a
System <none>
2017-09-19 03:55:18 +0000

    Checkpoint

--- datestuff.js
+++ datestuff.js
@@ -1,4 +1,27 @@
-module.exports = function mymodule(a, callback) {
+module.exports = function (request, response) {
+  var query = parseInt(request.params.query, 10);
   
-  return callback(null, a + ' hiya!');
+  if (!isNaN(query) && query.toString().length === 10) {
+    //if it's a 10 digit number
+    var type = '10 digit number'
+    var date = new Date(query * 1000);
+    var unixtime = query;
+  } else {
+    var type = 'not 10 digit number';
+    var date = new Date(request.params.query)
+    var unixtime = date.getTime() / 1000;
+  }
+  var mm = date.getMonth() + 1;
+  var dd = date.getDate();
+  var yyyy = date.getFullYear();  
+
+  var arr = {
+    //'type': type,
+    //'length': request.params.query.length,
+    'unixtime': unixtime,
+    'translated unixtime': mm + "-" + dd + "-" + yyyy,
+    //'normietime': normietime,//normietime.getMonth() + "--" + normietime.getDate() + "-" + normietime.getFullYear(),
+    //'module test here': moduletest
+  };
+  response.end(JSON.stringify(arr));
 }
--- server.js
+++ server.js
@@ -14,36 +14,7 @@ var datestuff = require('./datestuff.js')
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/:query", function (request, response) {
-  var query = parseInt(request.params.query, 10);
-  
-  if (!isNaN(query) && query.toString().length === 10) {
-    //if it's a 10 digit number
-    var type = '10 digit number'
-    var date = new Date(query * 1000);
-    var unixtime = query;
-  } else {
-    var type = 'not 10 digit number';
-    var date = new Date(request.params.query)
-    var unixtime = date.getTime() / 1000;
-  }
-  var mm = date.getMonth() + 1;
-  var dd = date.getDate();
-  var yyyy = date.getFullYear();  
-  var moduletest = datestuff("sent parameter", function callback(err, data) {
-    if (err) throw err;
-    return data;
-  });
-  var arr = {
-    //'type': type,
-    //'length': request.params.query.length,
-    'unixtime': unixtime,
-    'translated unixtime': mm + "-" + dd + "-" + yyyy,
-    //'normietime': normietime,//normietime.getMonth() + "--" + normietime.getDate() + "-" + normietime.getFullYear(),
-    //'module test here': moduletest
-  };
-  response.end(JSON.stringify(arr));
-});
+app.get("/:query", datestuff);
 
 app.get("/*", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
